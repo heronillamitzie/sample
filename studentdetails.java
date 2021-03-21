cbb0f6b037bef005261011cb35fd41d477cd7d03
heronillamitzie <79386687+heronillamitzie@users.noreply.github.com>
2021-03-21 13:58:07 +0800

    StudDetailss

--- studentdetails.java
+++ studentdetails.java
@@ -10,6 +10,13 @@ public static void main(string[]args){
 	students[0].marks = 80;
 	//method 3
 	students[0].section = 'A';
+         //method 7
+        students[2] = new Student();
+        students[2].name = "Mitzie";
+         //method 8
+        students[2].marks = 85;
+         //method 9
+        students[2].section = 'A';
 
 
 
